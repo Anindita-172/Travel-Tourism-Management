e984b8bb0a3f11d204c5748a89ee3f0f8518e579
Anindita-172 <82523510+Anindita-172@users.noreply.github.com>
2023-06-15 00:04:03 +0530

    Add files via upload

---
+++ Travel Management System/src/travel/management/system/Conn.java
@@ -0,0 +1,23 @@
+
+package travel.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    Conn()
+    {
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+         c= DriverManager.getConnection("jdbc:mysql:///travelmanagementsystem", "root", "Biba@1702"); 
+         s=c.createStatement();
+           
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }
+    
+}
