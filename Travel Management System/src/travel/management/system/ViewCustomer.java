7044a690535082d6f11f4748a21f94cea09613fb
Anindita-172 <82523510+Anindita-172@users.noreply.github.com>
2023-06-20 12:05:55 +0530

    ViewCustomer.java
    connected with the database

--- Travel Management System/src/travel/management/system/ViewCustomer.java
+++ Travel Management System/src/travel/management/system/ViewCustomer.java
@@ -3,10 +3,12 @@ package travel.management.system;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
-public class ViewCustomer extends JFrame {
+
+public class ViewCustomer extends JFrame implements ActionListener {
    JButton back;
     
     ViewCustomer(String username)
@@ -59,54 +61,95 @@ public class ViewCustomer extends JFrame {
      setVisible(true);
      
      JLabel lblcountry = new JLabel("Country");
-     lblcountry.setBounds(290,50,150,25);
+     lblcountry.setBounds(500,50,150,25);
      add(lblcountry);
      
      JLabel labelcountry = new JLabel();
-     labelcountry.setBounds(350,50,150,25);
+     labelcountry.setBounds(690,50,150,25);
      add(labelcountry);
      
      JLabel lbladdress = new JLabel("Address");
-     lbladdress.setBounds(290,110,150,25);
+     lbladdress.setBounds(500,110,150,25);
      add(lbladdress);
      
      JLabel labeladdress = new JLabel();
-     labeladdress.setBounds(350,110,150,25);
+     labeladdress.setBounds(690,110,150,25);
      add(labeladdress);
      
      JLabel lblemail = new JLabel("Email");
-     lblemail.setBounds(290,170,150,25);
+     lblemail.setBounds(500,170,150,25);
      add(lblemail);
      
      JLabel labelemail = new JLabel();
-     labelemail.setBounds(350,170,150,25);
+     labelemail.setBounds(690,170,150,25);
      add(labelemail);
      
      JLabel lblphone = new JLabel("Phone");
-     lblphone.setBounds(290,230,150,25);
+     lblphone.setBounds(500,230,150,25);
      add(lblphone);
      
      JLabel labelphone = new JLabel();
-     labelphone.setBounds(350,230,150,25);
+     labelphone.setBounds(690,230,150,25);
      add(labelphone);
      
      back = new JButton("Back");
      back.setBackground(Color.RED);
      back.setForeground(Color.WHITE);
      back.setBounds(350,350,100,25);
+     back.addActionListener(this);
      add(back);
      
-        ImageIcon i1 =  new ImageIcon(ClassLoader.getSystemResource("icons/viewall.png"));
+        ImageIcon i1 =  new ImageIcon(ClassLoader.getSystemResource("icons/viewall.jpg"));
         Image i2 = i1.getImage().getScaledInstance(400,500,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         image.setBounds(22,400,600,200);
         add(image);
      
+        
+        ImageIcon i4 =  new ImageIcon(ClassLoader.getSystemResource("icons/viewall.jpg"));
+        Image i5 = i4.getImage().getScaledInstance(400,500,Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel image2 = new JLabel(i6);
+        image2.setBounds(22,400,300,200);
+        add(image2);
+        
+        
+        try{
+        Conn c = new Conn();
+        String query = "select * from customer where username = '"+username+"'";
+        ResultSet rs = c.s.executeQuery(query);
+        while(rs.next())
+        {
+        labelusername.setText(rs.getString("username"));
+        labelid.setText(rs.getString("id"));
+        labelnumber.setText(rs.getString("number"));
+        labelname.setText(rs.getString("name"));
+        labelgender.setText(rs.getString("gender"));
+        labelcountry.setText(rs.getString("country"));
+        labeladdress.setText(rs.getString("address"));
+        labelphone.setText(rs.getString("phone"));
+        labelemail.setText(rs.getString("email"));
+        
+        
+        
+        }
+            
+        
+        
+        }catch(Exception e)
+        {e.printStackTrace();}
+        
+        
+        
      setVisible(true);
     
     }
+     public void actionPerformed(ActionEvent ae)
+    {
+      setVisible(false);
+    }
     public static void main(String[] args)
-    {new ViewCustomer("");
+    {new ViewCustomer("animuk1702");
     }
 }
